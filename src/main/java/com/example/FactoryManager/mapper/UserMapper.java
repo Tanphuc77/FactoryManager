daa69f5cf30e14386eefd8efb43a4e14c3d07be2
TanPhuc77 <tanphuc77@gmail.com>
2025-04-17 16:04:53 +0700

    update usermapper

--- src/main/java/com/example/FactoryManager/mapper/UserMapper.java
+++ src/main/java/com/example/FactoryManager/mapper/UserMapper.java
@@ -5,11 +5,14 @@ import com.example.FactoryManager.dto.response.UserDetailResponse;
 import com.example.FactoryManager.dto.response.UserResponse;
 import com.example.FactoryManager.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     User toUser(UserCreateRequest userCreateRequest);
+    @Mapping(source = "role.name", target = "roleName")
     UserResponse toUserResponse(User user);
+    @Mapping(source = "role.name", target = "roleName")
     UserDetailResponse toUserDetailResponse(User user);
 
 }

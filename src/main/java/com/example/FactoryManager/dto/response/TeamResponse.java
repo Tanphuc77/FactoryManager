dc2ce256459c5750000c682632c698c2761259da
INT-THQUAN-LAPT\ASUS ROG <trinhhungquan12345@gmail.com>
2025-04-23 09:13:39 +0700

    Add teamId Response

--- src/main/java/com/example/FactoryManager/dto/response/TeamResponse.java
+++ src/main/java/com/example/FactoryManager/dto/response/TeamResponse.java
@@ -10,5 +10,7 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class TeamResponse {
 
+    String teamId;
+
     String name;
 }
--- src/main/java/com/example/FactoryManager/mapper/TeamMapper.java
+++ src/main/java/com/example/FactoryManager/mapper/TeamMapper.java
@@ -3,9 +3,11 @@ package com.example.FactoryManager.mapper;
 import com.example.FactoryManager.dto.response.TeamResponse;
 import com.example.FactoryManager.entity.Team;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface TeamMapper {
 
+    @Mapping(source = "id", target = "teamId")
     TeamResponse toTeamResponse(Team team);
 }
